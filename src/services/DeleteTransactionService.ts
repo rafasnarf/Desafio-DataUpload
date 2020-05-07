ffb45570e3721002e48a89645dff6882b69b927f
rafasnarf <snarfelcv@gmail.com>
2020-05-07 17:22:37 -0300

    Desafio concluido

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactiosRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactiosRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('This transaction does not exist.');
+    }
+
+    await transactiosRepository.remove(transaction);
   }
 }
 
